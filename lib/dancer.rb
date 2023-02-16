a022d3a178f08babf605be7bf45b766c8e762584
Brian <wamwangimathenge@gmail.com>
2023-02-16 09:39:45 +0300

    Completed assignment

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    include Dance
+    extend MetaDancing
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
